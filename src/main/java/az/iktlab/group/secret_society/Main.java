7a8481d6c07426927eb2f1673ff895cabe1678ee
ShakhMV <mammadov_shah00@mail.ru>
2022-02-24 18:31:28 +0400

    console class ready

---
+++ src/main/java/az/iktlab/group/secret_society/Main.java
@@ -0,0 +1,13 @@
+package az.iktlab.group.secret_society;
+
+import az.iktlab.group.secret_society.console.Console;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Console console = new Console();
+        console.setDirection();
+    }
+}
+
